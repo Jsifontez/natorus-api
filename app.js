52d8b39cf34fd29cfcd62e4e7fe9e0f82ee183cb
jsifontez <juan.sifontez.dev@gmail.com>
2022-06-13 17:48:55 -0400

    feat: Responding to URL parameters

--- app.js
+++ app.js
@@ -19,12 +19,42 @@ const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simpl
 
 // we use /api/v1/tours to versioning our API
 // in case of future breaking changes
-app.get('/api/v1/tours', (req, res) => {
+app.get('/api/v1/tours/', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: {
       tours
+    }
+  })
+})
+
+// we use ':id' to pass a param in url that will be attached
+// in the req.params object.
+// we can also add one that can be optional '/:otherId?'
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params)
+
+  // the elements in the params objects are all string
+  // we need to convert that id into a number
+  const id = req.params.id * 1
+  const tour = tours.find(el => el.id === id)
+
+  // checking if the id is greater than the tour existed
+  // also we want to check allways is the request of the user
+  // is valid or don't have malicious code
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    })
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
     }
   })
 })
