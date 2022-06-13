950b055325e3e6b6049441b101c101aeba24a976
jsifontez <juan.sifontez.dev@gmail.com>
2022-06-13 19:58:15 -0400

    feat: Add handler to patch request

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.get('/api/v1/tours/', (req, res) => {
 // in the req.params object.
 // we can also add one that can be optional '/:otherId?'
 app.get('/api/v1/tours/:id', (req, res) => {
-  console.log(req.params)
 
   // the elements in the params objects are all string
   // we need to convert that id into a number
@@ -77,6 +76,22 @@ app.post('/api/v1/tours', (req, res) => {
         tour: newTour
       }
     })
+  })
+})
+
+app.patch('/api/v1/tours/:id', (req,res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    })
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
   })
 })
 
