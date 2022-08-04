461a867f94f23d99aa51dd1e2e94908788732550
jsifontez <juan.sifontez.dev@gmail.com>
2022-08-03 22:29:43 -0400

    chore: Add middleware for unhandled routes

--- app.js
+++ app.js
@@ -13,15 +13,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json()) // to read the req object
 app.use(express.static(`${__dirname}/public`))
 
-// middleware that is goind to executed of second
-// this is a global middleware (is executed before any route)
-app.use((req, res, next) => {
-  console.log('Hello from the middleware')
-  // we need to execute de next() function or else
-  // we won't be able to finish our execution
-  next()
-})
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
   next()
@@ -29,6 +20,17 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter) // we use a router to handle all request for that path
 app.use('/api/v1/users', userRouter)
+
+/**
+ * We use 'all' method to use this router for every HTTP method
+ * We use the '*' all url possible
+ **/
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  })
+})
 
 // 4) START SERVER
 module.exports = app
