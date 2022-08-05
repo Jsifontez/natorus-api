a1f2052b2470f1e80c156e03310d7cffccc3690c
jsifontez <juan.sifontez.dev@gmail.com>
2022-08-05 19:16:34 -0400

    chore: Add handling errors in prod and dev phase

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,42 @@
+const sendErrorDev = (err, res) => {
+  // we send the most detailed information possible
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  })
+}
+
+const sendErrorProd = (err, res) => {
+  // we send very little nformation
+  // Operational, trusted error: send message to the client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    })
+  } else {
+    // Programming or other unknow error: don't leak error details
+
+    // 1) Log error
+    console.error('ERROR!!!!', err)
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    })
+  }
+}
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500
   err.status = err.status || 'error'
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  })
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res)
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res)
+  }
 }
