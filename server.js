31cf31cea9e1afa775c50f70e260af95dee7746f
jsifontez <juan.sifontez.dev@gmail.com>
2022-08-08 20:19:44 -0400

    chore: Add unhandled exception error handler

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config({ path: './.config.env' })
 
+process.on('uncaughtException', (err) => {
+  console.log('UNHANDLED EXCEPTION! Shutting down the app...')
+  console.log(`Error: ${err.name}. Message: ${err.message}`)
+  process.exit(1)
+})
+
 const app = require('./app')
 
 const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD)
