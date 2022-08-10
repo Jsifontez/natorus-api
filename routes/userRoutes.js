e4bf2b8fdb2e4148bcc0c22cb69051f190003dee
jsifontez <juan.sifontez.dev@gmail.com>
2022-08-10 00:00:46 -0400

    feat: Add create new users functionallity

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const userController = require('../controllers/userController')
+const authController = require('../controllers/authController')
 
 // 3) ROUTES
 // this create a new router middleware to isolate and
 // create a sort of sub-app.
 // This process is called: Mounting the router
 const router = express.Router()
+
+router.post('/signup', authController.signup)
 
 router
   .route('/')
